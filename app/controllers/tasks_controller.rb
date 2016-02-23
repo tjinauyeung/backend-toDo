7613e39793919196003f6df1df7a56951aca397d
rooserik <rooserik@gmail.com>
2016-02-23 14:33:35 +0100

    completed all rspec task controller tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,33 @@ class TasksController < ApplicationController
       tasks: Task.all
     }
   end
+
+  def new
+    render json:{task: Task.new}
+  end
+
+  def create
+    render json:{task: Task.create(task_params)}
+  end
+
+  def edit
+    render json:{task: Task.find(params[:id])}
+  end
+
+  def update
+    task=Task.find(params[:id])
+    render json:{task: task.update(task_params)}
+
+  end
+
+  def destroy
+    task=Task.find(params[:id])
+    task.destroy
+    render json: { destroyed: true }
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
