570b956b879cd6d234b6f36972bdb6e2f3769800
rooserik <rooserik@gmail.com>
2016-02-23 16:25:52 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+tasks = Task.create([{description: "This needs doing"},{description: "buy bread"}])
