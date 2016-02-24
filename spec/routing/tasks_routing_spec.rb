68eac073322d0d0ddb4b2507aada02a43287c2a0
rooserik <rooserik@gmail.com>
2016-02-24 14:48:53 +0100

    created projects controller and a first index test on the routes spec test, also created a nested routing with projects and tasks

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -1,42 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :routing do
-  let(:task) {Task.create(description: "this is a description")}
+  project=FactoryGirl.create(:project)
+  task=FactoryGirl.create(:task)
   describe "routing" do
 
-  it "routes to #index" do
-      expect(:get => "/tasks").to route_to("tasks#index")
+    it "routes to #index of projects" do
+      expect(:get => "/projects").to route_to("projects#index")
     end
-
-    it "routes to #create" do
-      expect(:post => "/tasks").to route_to("tasks#create")
-    end
-
-    it "routes to #edit" do
-      task
-      expect(:get => "/tasks/1/edit").to route_to("tasks#edit", id: '1')
-
-    end
-
-    it "routes put update" do
-      task
-      expect(:put => "/tasks/1").to route_to("tasks#update", id: '1')
-
-    end
-
-    it "routes to #new" do
-      expect(:get => "/tasks/new").to route_to("tasks#new")
-    end
-
-    it "routes to #delete" do
-      task
-      expect(:delete => "/tasks/1").to route_to("tasks#destroy", id: '1')
-
-    end
-
-    it "does not routes to #show" do
-      task
-      expect(:get => "/tasks/1").not_to be_routable  
-    end
+#   it "routes to #index" do
+#       expect(:get => "/projects/:project_id/tasks").to route_to("tasks#index")
+#     end
+#
+#     it "routes to #create" do
+#       expect(:post => "/tasks").to route_to("tasks#create")
+#     end
+#
+#     it "routes to #edit" do
+#       task
+#       expect(:get => "/tasks/1/edit").to route_to("tasks#edit", id: '1')
+#
+#     end
+#
+#     it "routes put update" do
+#       task
+#       expect(:put => "/tasks/1").to route_to("tasks#update", id: '1')
+#
+#     end
+#
+#     it "routes to #new" do
+#       expect(:get => "/tasks/new").to route_to("tasks#new")
+#     end
+#
+#     it "routes to #delete" do
+#       task
+#       expect(:delete => "/tasks/1").to route_to("tasks#destroy", id: '1')
+#
+#     end
+#
+#     it "does not routes to #show" do
+#       task
+#       expect(:get => "/tasks/1").not_to be_routable
+#     end
   end
 end
