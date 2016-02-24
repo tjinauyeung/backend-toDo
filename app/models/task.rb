a08683541b23305bfdca51b72bcf2d7e9679334d
navase <margot.sauter@gmail.com>
2016-02-24 14:04:32 +0100

    no tests yet

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :project
   validates :done, inclusion: [true, false]
-  validates :project_id, presence: true
+  #validates :project_id, presence: true
 end
