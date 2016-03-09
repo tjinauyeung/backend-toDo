b91161405f459c21c4501db849847133172ef093
Tjin Au Yeung <tjinauyeung@gmail.com>
2016-03-09 13:26:08 +0100

    added bitballoon link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class Backend::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
-      origins 'localhost:3000', 'sentry-daisy-65516.bitballoon.com'
+      origins 'localhost:3000', 'sentry-daisy-65516.bitballoon.com', 'chef-douglas-43423.bitballoon.com'
 
       resource '/cors',
         headers: :any,
